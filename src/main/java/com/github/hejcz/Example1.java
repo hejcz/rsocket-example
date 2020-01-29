0ac0dcbccdd2aa1ee58dfa8388a36c34b687b18f
Julian Rubin <julian.rubin@rtbhouse.com>
2020-01-29 10:26:56 +0100

    Messages example

--- src/main/java/com/github/hejcz/Example1.java
+++ src/main/java/com/github/hejcz/Example1.java
@@ -1,9 +1,15 @@
 package com.github.hejcz;
 
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
@@ -17,18 +23,78 @@ import io.rsocket.util.ByteBufPayload;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+interface Message {
+    String name();
+}
+
+class ImportantMessage implements Message {
+
+    @Override
+    public String name() {
+        return "important";
+    }
+}
+
+class NotImportantMessage implements Message {
+
+    @Override
+    public String name() {
+        return "not-important";
+    }
+}
+
+/**
+ * This example emits important and not important messages. It drops not
+ * important messages on backpressure and stores dropped important messages in
+ * intermediate collection. Then it publishes dropped important messages
+ * with fixed interval.
+ */
 public class Example1 {
 
     public static void main(String[] args) throws InterruptedException {
-        AtomicInteger dropped = new AtomicInteger();
-
+        ScheduledExecutorService es = Executors.newScheduledThreadPool(1);
         AbstractRSocket handler = new AbstractRSocket() {
+            AtomicBoolean skipNotImportant = new AtomicBoolean(false);
+            Random random = new Random();
+            Set<Message> droppedImportant = new HashSet<>();
+
             @Override
             public Flux<Payload> requestStream(Payload payload) {
-                return Flux.interval(Duration.ofMillis(10))
-                        .map(it -> ByteBufPayload.create("hello from server"))
-                        .onBackpressureDrop(p -> System.out.println(
-                                "dropped " + dropped.incrementAndGet()));
+                return Flux.interval(Duration.ofMillis(5))
+                    .map(
+                        it -> random.nextInt(10) < 8
+                            ? new NotImportantMessage()
+                            : new ImportantMessage())
+                    .filter(
+                        it -> !skipNotImportant.get()
+                            || isImportant(it))
+                    .mergeWith(
+                        Flux.interval(Duration.ofSeconds(1))
+                            .switchMap(ignored -> getPublisher()))
+                    .onBackpressureDrop(m -> {
+                        if (isImportant(m)) {
+                            droppedImportant.add(m);
+                        }
+                        if (!skipNotImportant.getAndSet(true)) {
+                            es.schedule(() -> skipNotImportant.set(false),
+                                500, TimeUnit.MILLISECONDS);
+                        }
+                    })
+                    .map(it -> ByteBufPayload.create(it.name()));
+            }
+
+            private Publisher<? extends Message> getPublisher() {
+                if (droppedImportant.isEmpty()) {
+                    return Flux.empty();
+                }
+                System.out.println("REWIND: " + droppedImportant.size());
+                HashSet<Message> dropped = new HashSet<>(droppedImportant);
+                droppedImportant.removeAll(dropped);
+                return Flux.fromIterable(dropped);
+            }
+
+            private boolean isImportant(Message m) {
+                return m.name().equals("important");
             }
         };
 
@@ -54,14 +120,14 @@ public class Example1 {
                     @Override
                     public void onSubscribe(Subscription s) {
                         this.s = s;
-                        s.request(10L);
+                        s.request(200L);
                     }
 
                     @Override
                     public void onNext(Payload payload) {
                         System.out.println(payload.getDataUtf8());
                         try {
-                            Thread.sleep(random.nextInt(20));
+                            Thread.sleep(random.nextInt(30));
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -80,6 +146,8 @@ public class Example1 {
                 });
 
         Thread.sleep(10_000);
+        es.shutdownNow();
+        es.awaitTermination(10, TimeUnit.SECONDS);
     }
 
 }
